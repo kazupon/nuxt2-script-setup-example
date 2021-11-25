658da73ce1ca5d2dd49108482e5be7b701ca5f62
kazuya kawaguchi <kawakazu80@gmail.com>
2021-11-26 01:02:44 +0900

    commit first

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent, useMeta } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'Index',
+  setup() {
+    useMeta({
+      title: 'My page',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'My custom description'
+        }
+      ]
+    })
+    return {}
+  },
+  head: {},
+})
+</script>
+
+<template>
+  <Tutorial/>
+</template>
+
